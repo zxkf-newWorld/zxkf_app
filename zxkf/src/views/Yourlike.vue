b5d82cbdc40112221a2d612a18dbdb5674c3032d
Hepeng <hepeng@baba.com>
2019-08-16 08:14:29 +0800

    首页猜你喜欢静态数据完成

--- zxkf/src/views/Yourlike.vue
+++ zxkf/src/views/Yourlike.vue
@@ -35,10 +35,53 @@
                 <li class="good-item">
                     <div class="house-card">
                         <div class="base-info">
-                            <img src="../assets/like/like4.png" alt="">
-
+                            <div class="main-photo">
+                                <img src="../assets/like/like4.png"/>
+                                <div class="live-photo">平台实拍 · 8</div>
+                            </div>
+                            <div class="detail">
+                                <p class="title">
+                                    <span>合租</span>
+                                    <span>川杨新苑四期</span>
+                                </p>
+                                <p class="room-info">25M² | 6/20层 | 朝西北-次卧</p>
+                                <p class="lables">
+                                    <span>独卫</span>
+                                    <span>新上架</span>
+                                    <span>电梯房</span>
+                                    <span>非一楼</span>
+                                </p>
+                            </div>
                         </div>
-                        <div class="extra-info"></div>
+                        <div class="extra-info">
+                            <div class="landlord-info">
+                                <div class="owner">
+                                    <div class="logo">
+                                    </div>
+                                    <div class="name">朱先生</div>
+                                    <div class="tag"></div>
+                                </div>
+                                <div class="score_comment">
+                                    <div class="score">
+                                        评分：
+                                        <b>4.5</b>
+                                    </div>
+                                    <div class="comment_number">125条评论</div>
+                                </div>
+                                <em>"</em>
+                                <div class="one_comment">干净舒适</div>
+                                <em>"</em>
+                            </div>
+                            <div class="price-info">
+                                <div class="price">
+                                    <span>￥2546</span>"/月"
+                                </div>
+                                <div class="shuidianmeiwang-text">
+                                    <span>水电煤网费涨就赔</span>
+                                    <span>&nbsp;随时入住</span>
+                                </div>
+                            </div>
+                        </div>   
                     </div>
                 </li>
                 <li class="good-item">
@@ -47,7 +90,35 @@
                             <img src="../assets/like/like5.png" alt="">
 
                         </div>
-                        <div class="extra-info"></div>
+                        <div class="extra-info">
+                            <div class="landlord-info">
+                                <div class="owner">
+                                    <div class="logo">
+                                    </div>
+                                    <div class="name">朱先生</div>
+                                    <div class="tag"></div>
+                                </div>
+                                <div class="score_comment">
+                                    <div class="score">
+                                        评分：
+                                        <b>4.5</b>
+                                    </div>
+                                    <div class="comment_number">125条评论</div>
+                                </div>
+                                <em>"</em>
+                                <div class="one_comment">干净舒适</div>
+                                <em>"</em>
+                            </div>
+                            <div class="price-info">
+                                <div class="price">
+                                    <span>￥2546</span>"/月"
+                                </div>
+                                <div class="shuidianmeiwang-text">
+                                    <span>水电煤网费涨就赔</span>
+                                    <span>&nbsp;随时入住</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </li>
 
