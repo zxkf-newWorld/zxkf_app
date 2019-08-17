4db192d7a75f18ee59e32180b60c9ce3e0d7886c
Hepeng <Hepeng>
2019-08-17 13:25:36 +0800

    首页页面的完成

--- zxkf/src/views/Yourlike.vue
+++ zxkf/src/views/Yourlike.vue
@@ -5,7 +5,7 @@
                 <span>猜你喜欢</span>
             </div>
             <ul class="good-list">
-                <li class="good-item">
+                <!-- <li class="good-item">
                     <div class="house-card">
                         <div class="base-info">
                             <img src="../assets/like/like1.png" alt="">
@@ -13,8 +13,8 @@
                         </div>
                         <div class="extra-info"></div>
                     </div>
-                </li>
-                <li class="good-item">
+                </li> -->
+                <!-- <li class="good-item">
                     <div class="house-card">
                         <div class="base-info">
                             <img src="../assets/like/like2.png" alt="">
@@ -31,7 +31,7 @@
                         </div>
                         <div class="extra-info"></div>
                     </div>
-                </li>
+                </li> -->
                 <li class="good-item">
                     <div class="house-card">
                         <div class="base-info">
@@ -46,10 +46,10 @@
                                 </p>
                                 <p class="room-info">25M² | 6/20层 | 朝西北-次卧</p>
                                 <p class="lables">
-                                    <span>独卫</span>
-                                    <span>新上架</span>
-                                    <span>电梯房</span>
-                                    <span>非一楼</span>
+                                    <span class="lable">独卫</span>
+                                    <span class="lable">新上架</span>
+                                    <span class="lable">电梯房</span>
+                                    <span class="lable">非一楼</span>
                                 </p>
                             </div>
                         </div>
@@ -68,13 +68,11 @@
                                     </div>
                                     <div class="comment_number">125条评论</div>
                                 </div>
-                                <em>"</em>
-                                <div class="one_comment">干净舒适</div>
-                                <em>"</em>
+                                <div class="one_comment">"干净舒适"</div>
                             </div>
                             <div class="price-info">
                                 <div class="price">
-                                    <span>￥2546</span>"/月"
+                                    <span class="number">￥2546</span>/月
                                 </div>
                                 <div class="shuidianmeiwang-text">
                                     <span>水电煤网费涨就赔</span>
@@ -84,7 +82,7 @@
                         </div>   
                     </div>
                 </li>
-                <li class="good-item">
+                <!-- <li class="good-item">
                     <div class="house-card">
                         <div class="base-info">
                             <img src="../assets/like/like5.png" alt="">
@@ -120,7 +118,7 @@
                             </div>
                         </div>
                     </div>
-                </li>
+                </li> -->
 
             </ul>
         </div>
@@ -150,6 +148,218 @@ export default {
         font-weight: 700;
         text-align: left;
     }
+    .house-card,.extra-info{
+        margin: 0 .3rem;
+        border-bottom: 1px solid #ececec;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+    }   
+    .house-card .base-info{
+        width: 100%;
+        display: flex;
+        box-orient: horizontal;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: flex-start;
+    }
+    /* 猜你喜欢商品图片展示 */
+    .house-card .base-info .main-photo{
+        position: relative;
+        margin: .4rem .3rem .4rem 0;
+        width: 2.1rem;
+        height: 1.6rem;
+        border-radius: .08rem;
+        overflow: hidden;
+        background-size: 100% 100%;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        -ms-flex-negative: 0;
+        flex-shrink: 0;
+    }
+    /* 商品图片的底部在线实拍的信息展示 */
+    .house-card .base-info .main-photo .live-photo{
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: .34rem;
+        line-height: .34rem;
+        font-size: .2rem;
+        text-align: center;
+        color: #fff;
+        background-color: rgba(28,28,28,.4);
+        border-bottom-left-radius: .08rem;
+        border-bottom-right-radius: .08rem;
+    }
+    /* 商品基础信息 */
+    .house-card .base-info .detail{
+        width: 4.2rem;
+        padding: 0;
+        margin: 0;
+        outline: none;
+    }
+    /* 商品信息标题*/
+    .house-card .base-info .detail .title{
+        font-size: .34rem;
+        color: #1c1c1c;
+        font-weight: 700;
+        margin-top: .2rem;
+        line-height: .54rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    .house-card .base-info .detail .room-info{
+        color: #1c1c1c;
+        font-size: .28rem;
+        line-height: .48rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        text-align: right;
+
+    }
+    .house-card .base-info .detail .lables{
+        height: .38rem;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-orient: horizontal;
+        -webkit-box-direction: normal;
+        -ms-flex-direction: row;
+        flex-direction: row;
+        -ms-flex-wrap: nowrap;
+        flex-wrap: nowrap;
+        -webkit-box-pack: start;
+        -ms-flex-pack: start;
+        justify-content: flex-end;
+        margin-top: .1rem;
+    }
+    .house-card .base-info .detail .lables .lable{
+        height: 100%;
+        font-size: .22rem;
+        line-height: .38rem;
+        margin-right: .1rem;
+        color: #9b9b9b;
+        background-color: #f6f6f6;
+        display: inline-block;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        padding: 0 .02rem;
+        border-radius: .02rem;
+    }
+    .extra-info{
+        display: flex;
+        justify-content: space-between;
+    }
+    /* 房东信息 */
+    .extra-info .landlord-info{
+        margin-left: -10px;
+    }
+    .extra-info .landlord-info .owner{
+        display: flex;
+        align-items: center;
+        -webkit-box-align: center;
+        margin-left: -.2rem;
+    }
+    .extra-info .landlord-info .owner .logo{
+        width: .48rem;
+        height: .48rem;
+        margin-left: 0px;
+        background-image: url(../assets/like/baletu.jpg);
+        background-size: 100% 100%;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        border-radius: 50%;
+        margin-right: .12rem;
+        overflow: hidden;
+    }
+    .extra-info .landlord-info .owner .name{
+        display: inline-block;
+        color: #1c1c1c;
+        font-size: .24rem;
+        line-height: .48rem;
+        margin-right: .12rem;
+    }
+    .extra-info .landlord-info .owner .tag{
+        width: .44rem;
+        height: .24rem;
+        background-image: url(../assets/like/host.png);
+        background-size: 100% 100%;
+        background-position: 50%;
+        background-repeat: no-repeat;
+    }
+    /* 房东信息评分栏 */
+    .house-card .extra-info .landlord-info .score_comment {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        margin-top: .1rem;
+        margin-left: -0.2rem;
+    }
+    .house-card .extra-info .landlord-info .score_comment .score{
+        color: #1c1c1c;
+        font-size: .22rem;
+        margin-right: .16rem;
+    }
+    .house-card .extra-info .landlord-info .score_comment .comment_number{
+        color: #9b9b9b;
+        font-size: .22rem;
+    }
+    /* 商品评论 */
+    .house-card .extra-info .landlord-info .one_comment{
+        margin-top: .1rem;
+        margin-left: -5.8rem;
+        display: inline-block;
+        max-width: 2.4rem;
+        color: gray;
+        font-size: .22rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    .house-card .extra-info .landlord-info .price-info{
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-orient: vertical;
+        -webkit-box-direction: normal;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        -webkit-box-align: end;
+        -ms-flex-align: end;
+        align-items: flex-end;
+    }
+    .house-card .extra-info .price-info .price{
+        /* display: flex; */
+        color: #ee3943;
+        font-size: .24rem;
+        text-align: right ;
+   }
+    .house-card .extra-info .price-info .price span{
+        
+    }
+    .house-card .extra-info .price-info .price .number{
+        font-size: .32rem;
+        font-weight: 700;
+    }
+    .house-card .extra-info .price-info .shuidianmeiwang-text{
+        color: #f58f32;
+        font-size: .28rem;
+        font-weight: 700;
+        margin-top: .1rem;
+        max-width: 4rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    
+
+
+
     .good-list{
         list-style: none;
     }
