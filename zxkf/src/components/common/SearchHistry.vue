70ef7df337c88a162e9cfa5c6ee21385dc42c2bc
Hepeng <Hepeng@baba.com>
2019-12-23 19:16:53 +0800

    搜索栏优化

---
+++ zxkf/src/components/common/SearchHistry.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="search-history">
+        <div class="searchHistory">
+            <!-- 标题 -->
+            <div>
+                <div>历史搜索</div>
+                <div>
+                    <!-- 删除标志 -->
+                </div>
+            </div>
+            <!-- 搜索历史 -->
+            <ul>
+                <li>
+                    <div>搜索图标</div>
+                    <div>
+                        <p>搜索关键字</p>
+                        <p>品牌公寓-全部户型</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="hotSearch"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'searchHistory',
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
